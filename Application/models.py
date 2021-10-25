2702be29ec1a65358ed21c6b9478449e092cddfe
= <=>
2021-10-24 21:01:00 -0500

    added models for user & course

--- Application/models.py
+++ Application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    firstname = models.CharField(max_length=30, default='')
+    lastname = models.CharField(max_length=30, default='')
+    email = models.EmailField(max_length=100, default='')
+    password = models.CharField(max_length=30, default='')
+    role = models.CharField(max_length=10, default='') #admin, professor, student, alumni, 
+    
+    
+class course(models.Model):
+    name = models.CharField(max_length=40, default='')
+    professor = models.ForeignKey(user, on_delete=models.CASCADE)
