cf2c5308f9be79c2d8d7cd134041a448af8b4122
anyamikulo <80342404+anyamikulo@users.noreply.github.com>
2021-10-27 20:51:44 -0500

    getting fields to display on sign up and forgot password

--- Application/views.py
+++ Application/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import LoginForm
+from .forms import LoginForm, ForgotPassForm, SignupForm
 from .models import User
 
 
@@ -17,8 +17,10 @@ def home(request):
 def user_profile(request):
     return render(request, 'profile.html')
 
+
 def sign_up(request):
-    return render(request, 'sign_up.html')
+    return render(request, 'sign_up.html', {'form': SignupForm()})
+
 
 def forgot_password(request):
-    return render(request, 'forgot_password.html')
+    return render(request, 'forgot_password.html', {'form': ForgotPassForm()})
