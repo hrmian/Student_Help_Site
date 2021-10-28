24c8552af057ebb73209f08ae471b7c9fa48a3f1
hamza <hrmian@uwm.edu>
2021-10-28 01:32:00 -0500

    drop down

--- Application/forms.py
+++ Application/forms.py
@@ -3,7 +3,8 @@ from django.contrib.auth import authenticate
 from django.forms import PasswordInput
 from Application.models import Reply, Topic, User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import ROLES
+from django.db import models
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100, required=True)
@@ -28,7 +29,7 @@ class SignUpForm(forms.ModelForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "First Name"}), max_length=100, required=True)
     last_name = forms.CharField(widget= forms.TextInput(attrs={"placeholder": "Last Name"}), max_length=100, required=True)
     email = forms.CharField(widget= forms.TextInput(attrs={"placeholder": "Email"}), max_length=20, required=True)
-    role = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Role"}), max_length=100, required=True)
+    role = models.CharField(max_length=30, choices=ROLES, default='Alum')
     username = forms.CharField(widget= forms.TextInput(attrs={"placeholder": "Username"}), max_length=100, required=True)
     password = forms.CharField(widget=PasswordInput(attrs={"placeholder": "Password"}), min_length=8, max_length=100, required=True)
     confirm_password = forms.CharField(widget=PasswordInput(attrs={"placeholder": "Confirm Password"}),min_length=8, max_length=100, required=True)
