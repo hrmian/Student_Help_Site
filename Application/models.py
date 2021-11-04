1e01ae72e88bc80cb7e6b161bf289c901d6ac88e
mdthal <matthewdthal@gmail.com>
2021-11-04 14:46:11 -0500

    add timestamp to thread model

--- Application/models.py
+++ Application/models.py
@@ -29,6 +29,7 @@ class Course(models.Model):
 
 class Thread(models.Model):
     subject = models.CharField(max_length=40)
+    timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
