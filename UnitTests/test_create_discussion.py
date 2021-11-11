fceeefefc1525aae5493bd77c552f9d93b7d64f7
hamza <hrmian@uwm.edu>
2021-11-11 16:12:11 -0600

    unit test directory

--- UnitTests/test_create_discussion.py
+++ UnitTests/test_create_discussion.py
@@ -1,9 +1,19 @@
 import unittest
+from django.test import TestCase
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Help_Site.settings')
+import django
+django.setup()
+from Application.models import Thread, Post
 
 
 class MyTestCase(unittest.TestCase):
+    #def setUp(self):
+
+
+
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
