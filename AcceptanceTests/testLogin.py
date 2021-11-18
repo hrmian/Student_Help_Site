6983d59c5dc3b9ed5af1ed56a65af303bd653102
hamza <hrmian@uwm.edu>
2021-11-18 16:23:16 -0600

    Unit & acceptence start

---
+++ AcceptanceTests/testLogin.py
@@ -0,0 +1,21 @@
+from django.test import TestCase, Client
+from Application.models import User
+
+
+class TestLogin(TestCase):
+    def setUp(self):
+        user = User(username="user1", email="user1@gmail.com")
+        user.set_password("12345678")
+        user.save()
+        self.user = user
+        self.client = Client()
+
+    def test_login(self):
+        response = self.client.post('/', {'j_username': self.user.email, 'j_password': self.user.password})
+        self.assertEqual(response.url, '/accounts/login/?next=/',
+                         'Incorrect redirect after the existing user logged in')
+        response = self.client.get('/accounts/login/?next=/')
+
+    def test_invalidLogin(self):
+        response = self.client.post('/', {'j_username': self.user.email, 'j_password': 4215165}, follow=True)
+        self.assertTrue(response.status_code == 200)
--- UnitTests/test_create_discussion.py
+++ UnitTests/test_create_discussion.py
@@ -1,20 +1,49 @@
-import unittest
 from django.test import TestCase
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Help_Site.settings')
-import django
-django.setup()
-from Application.models import Thread, Post
+from django.urls import reverse
+from Application.models import User, Course, Thread, Post
 
 
-class MyTestCase(unittest.TestCase):
-    #def setUp(self):
+class TestCreateDiscussion(TestCase):
+    def setUp(self):
+        user = User(username="user1", email="user1@gmail.com")
+        user.set_password("12345678")
+        user.save()
+        self.course = Course.objects.create(name="course_test_1", professor=user)
+        self.course_2 = Course.objects.create(name="course_test_2", professor=user)
+        self.user = user
+
+    def login(self):
+        self.client.login(username=self.user.username, password="12345678")
+
+    def test_validData(self):
+        self.login()
+        self.client.post(reverse("create_thread", args=[self.course.id]), {
+            "subject": "thread test"
+        })
+        self.client.post(reverse("create_thread", args=[self.course.id]), {
+            "subject": "thread test 2"
+        })
+
+        self.client.post(reverse("create_thread", args=[self.course_2.id]), {
+            "subject": "thread test 3"
+        })
+
+        self.assertEqual(Thread.objects.filter(course=self.course).count(), 2)
+        self.assertEqual(Thread.objects.filter(course=self.course_2).count(), 1)
+
+    def test_checkTitle(self):
+        self.login()
+        self.client.post(reverse("create_thread", args=[self.course.id]), {
+            "subject": "thread 1",
+            "content": "thread 1 content"
+        })
+
+        self.client.post(reverse("create_thread", args=[self.course.id]), {
+            "subject": "thread 2",
+            "content": "thread 2 content"
+        })
+
+        self.assertTrue(Thread.objects.filter(subject="thread 1").exists())
+        self.assertTrue(Thread.objects.filter(subject="thread 2").exists())
 
 
-
-    def test_something(self):
-        self.assertEqual(True, True)
-
-
-if __name__ == '__main__':
-    unittest.main()
